ef7bdda5aa8481bf58fac137bb6e5fbab7a6db6b
Alexander <asaulovalex@gmail.com>
2022-11-18 19:30:36 +0200

    finished @media for (M-H: 1200px) & (M-W: 1000px)

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,6 +3,7 @@ import { useState } from "react";
 
 export default function Header() {
     const [headerFixed, setHeaderFixed] = useState(false);
+    const [burgerMenuPressed, setBurgerMenuPressed] = useState(false);
 
     function headerHandler() {
         const innerHeight = window.innerHeight * 0.8;
@@ -15,14 +16,75 @@ export default function Header() {
 
     window.addEventListener("scroll", headerHandler);
 
+    // menu_btn.addEventListener("click", function () {
+    //     if (! menu_btn.classList.contains("active")){
+    //         // чтобы сделать актив = крестик
+    //         menu_btn.classList.add("active");
+    //         // присвоить актив для нав_2
+    //         menu_nav.classList.add("active");
+    //         header_slogan.classList.add("hide");
+    //         header.classList.add("red");
+    //         header_logo.classList.add("white");
+    //     }
+    //     else {
+    //         // чтобы убрать актив = три полоски
+    //         menu_btn.classList.remove("active");
+    //         // убрать актив для нав_2
+    //         menu_nav.classList.remove("active");
+    //         header_slogan.classList.remove("hide");
+    //         header.classList.remove("red");
+    //     }
+    // });
+
+    function toggleBurgerMenu() {
+        if (burgerMenuPressed) {
+            setBurgerMenuPressed(false);
+        } else {
+            setBurgerMenuPressed(true);
+        }
+    }
+    function closeBurgerMenu() {
+        if (burgerMenuPressed) {
+            setBurgerMenuPressed(false);
+        }
+    }
+
     return (
         <header className={headerFixed ? "header fixed" : "header"}>
-            <nav>
-                <a href="#about">About me</a>
-                <a href="#skills">Skills</a>
-                <a href="#portfolio">Portfolio</a>
-                <a href="#contacts">Contact me</a>
+            <nav className={burgerMenuPressed ? "active" : ""}>
+                <a
+                    href="#about"
+                    onClick={closeBurgerMenu}
+                >
+                    About me
+                </a>
+                <a
+                    href="#skills"
+                    onClick={closeBurgerMenu}
+                >
+                    Skills
+                </a>
+                <a
+                    href="#portfolio"
+                    onClick={closeBurgerMenu}
+                >
+                    Portfolio
+                </a>
+                <a
+                    href="#contacts"
+                    onClick={closeBurgerMenu}
+                >
+                    Contact me
+                </a>
             </nav>
+            {/* <!-- burger menu button --> */}
+            <div
+                class={burgerMenuPressed ? "nav_toggle active" : "nav_toggle"}
+                onClick={toggleBurgerMenu}
+            >
+                <span class="toggle_item"></span>
+            </div>
+            {/* <!-- burger menu button --> */}
         </header>
     );
 }
